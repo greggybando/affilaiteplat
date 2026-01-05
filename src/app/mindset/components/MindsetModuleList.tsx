82bcf165e874ce9eeeab671c22d34b51ed8e8a5c
Grant Lannin <grant@reelstacks.ai>
2026-01-05 12:23:09 -0800

    Fix missing closing div tag in notes section

--- src/app/mindset/components/MindsetModuleList.tsx
+++ src/app/mindset/components/MindsetModuleList.tsx
@@ -779,6 +779,7 @@ export function MindsetModuleList({ modules, categories, affiliate }: MindsetMod
                           </div>
                         </div>
                       )}
+                      </div>
                     </>
                   )
                 })()}
